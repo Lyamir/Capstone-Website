c07345ec6d46326ea6b44f6156f9dca6b82715b1
Tester <tester@testing.com>
2021-12-17 00:13:41 +0800

    changed testing scripts

--- test.js
+++ test.js
@@ -8,13 +8,55 @@ async function test(){
     let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
     await driver.get("http://localhost:8008");
     await driver.findElement(By.id("postCard-1")).click();
-    await driver.findElement(By.id("updatepost")).click();
-    await driver.back();
-    await driver.findElement(By.id("deletepost")).click();  
-    await driver.back();
-    await driver.findElement(By.id("commentauthor")).sendKeys("Tester Author");
-    await driver.findElement(By.id("commentarea")).sendKeys("Testing comment area");
-    await driver.findElement(By.id("commentsubmit")).click();
+    driver.findElement(By.id('updatepost')).then(function(webElement) {
+        console.log('Update exists');
+    }, function(err) {
+        if (err.state && err.state === 'no such element') {
+            console.log('Element not found');
+        } else {
+            webdriver.promise.rejected(err);
+        }
+    });
+    driver.findElement(By.id('deletepost')).then(function(webElement) {
+        console.log('Delete exists');
+    }, function(err) {
+        if (err.state && err.state === 'no such element') {
+            console.log('Element not found');
+        } else {
+            webdriver.promise.rejected(err);
+        }
+    });
+    driver.findElement(By.id('commentauthor')).then(function(webElement) {
+        console.log('Comment author exists');
+        webElement.sendKeys("Tester Author");
+    }, function(err) {
+        if (err.state && err.state === 'no such element') {
+            console.log('Element not found');
+        } else {
+            webdriver.promise.rejected(err);
+        }
+    });
+    //await driver.findElement(By.id("commentauthor")).sendKeys("Tester Author");
+    driver.findElement(By.id('commentarea')).then(function(webElement) {
+        console.log('Comment area exists');
+        webElement.sendKeys("Testing comment area");
+    }, function(err) {
+        if (err.state && err.state === 'no such element') {
+            console.log('Element not found');
+        } else {
+            webdriver.promise.rejected(err);
+        }
+    });
+    driver.findElement(By.id('commentsubmit')).then(function(webElement) {
+        console.log('Comment submit button exists');
+        webElement.click();
+    }, function(err) {
+        if (err.state && err.state === 'no such element') {
+            console.log('Element not found');
+        } else {
+            webdriver.promise.rejected(err);
+        }
+    });
     await driver.quit();
 }
 
