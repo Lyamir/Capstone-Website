3a4f4e134cf7608bf8543cea8347aa6a02347020
Tester <tester@testing.com>
2021-12-21 21:02:56 +0800

    comment out unnecessary function

--- test/test.js
+++ test/test.js
@@ -24,23 +24,23 @@ describe("Unit Tests", function(){
 
 
 
-async function test(){
-    let options = new firefox.Options();
-    options.addArguments("-headless");
-    let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
-    await driver.get("http://localhost:8008");
-    await driver.findElement(By.id("postCard-1")).click();
+// async function test(){
+//     let options = new firefox.Options();
+//     options.addArguments("-headless");
+//     let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+//     await driver.get("http://localhost:8008");
+//     await driver.findElement(By.id("postCard-1")).click();
 
-    let titleText = await driver.findElement(By.className("title")).getText().then(function(value){
-        return value;
-    });
+//     let titleText = await driver.findElement(By.className("title")).getText().then(function(value){
+//         return value;
+//     });
 
-    assert.strictEqual(titleText, "Goodbye WorldAAA");
+//     assert.strictEqual(titleText, "Goodbye WorldAAA");
 
-    await driver.findElement(By.id('commentauthor')).sendKeys("Tester author");
-    await driver.findElement(By.id("commentarea")).sendKeys("Testing comment area");
-    await driver.findElement(By.id('commentsubmit')).click();
+//     await driver.findElement(By.id('commentauthor')).sendKeys("Tester author");
+//     await driver.findElement(By.id("commentarea")).sendKeys("Testing comment area");
+//     await driver.findElement(By.id('commentsubmit')).click();
 
-    await driver.quit();
-}
-test();
+//     await driver.quit();
+// }
+// test();
