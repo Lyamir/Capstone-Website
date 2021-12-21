f5c76eead763c4e8d12697ad8e1cae46b1682937
Tester <tester@testing.com>
2021-12-22 01:08:09 +0800

    added mocha junit reporter

--- test/test.js
+++ test/test.js
@@ -3,20 +3,34 @@ const firefox = require("selenium-webdriver/firefox");
 const  assert = require("assert");
 const chai = require("chai");
 const chaihttp = require("chai-http");
+const router = require("../router/indexRouter");
+const post = require("../controller/postController");
 const app = require("../index");
 
 chai.should();
 chai.use(chaihttp);
 
 describe("Unit Tests", async function(){
+    let options = new firefox.Options();
+    options.addArguments("-headless");
+    let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+    await driver.get("http://localhost:8008");
+
     it("It should GET all posts", function(done){
         console.log("all posts is retrieved, some are updated or deleted");
-        chai.request(app)
-            .get("index")
-            .end((function(err, response){
-                response.should.have.status(200);
-                done();
-            }));
+        
+        await driver.findElement(By.id("postCard-1")).click();
+
+        let titleText = await driver.findElement(By.className("title")).getText().then(function(value){
+            return value;
+        });
+
+        assert.strictEqual(titleText, "Goodbye World");
+
+        await driver.findElement(By.id('commentauthor')).sendKeys("Tester author");
+        await driver.findElement(By.id("commentarea")).sendKeys("Testing comment area");
+        await driver.findElement(By.id('commentsubmit')).click();
+        done();
     });
 });
 
@@ -44,6 +58,8 @@ describe("System Tests", async function(){
         console.log("an activity is performed");
         done();
     });
+
+    await driver.quit();
 });
 
 
