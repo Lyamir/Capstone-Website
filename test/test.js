7a30e3b0586ef48baa16d0c7091af1ebb8641fe8
Tester <tester@testing.com>
2021-12-24 00:59:01 +0800

    fixed comment error

--- test/test.js
+++ test/test.js
@@ -1,8 +1,5 @@
 const {Builder, By, Key, ulit, WebDriver, WebElement} = require ("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
-const  assert = require("assert");
-const should = require("chai").should();
-const expect = require("chai").expect();
 
 let post = {
     title:"Integration Testing",
@@ -19,7 +16,6 @@ let testComment = {
 
 describe("Unit Tests", async function(){
     it("Homepage should GET all posts", async function(){
-        console.log("all posts is retrieved, some are updated or deleted");
         let options = new firefox.Options();
         options.addArguments("-headless");
         let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
@@ -32,7 +28,6 @@ describe("Unit Tests", async function(){
     });
 
     it("It should display a page", async function(){
-        console.log("a page is displayed with the expected output");
         let options = new firefox.Options();
         options.addArguments("-headless");
         let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
@@ -109,11 +104,9 @@ describe("Unit Tests", async function(){
         await driver.findElement(By.xpath("/html/body/div/div/form/div/input[1]")).sendKeys(testingUpdate);
         await driver.findElement(By.xpath("/html/body/div/div/form/div/button")).click();
 
-        console.log(await driver.findElement(By.xpath("/html/body/div/div/div[1]")).getText());
-
         //Test if updates are reflected
-        // let resultingTitle = await driver.findElement(By.xpath("/html/body/div/div/div[1]")).getText();
-        // resultingTitle.should.equal(testingUpdate);
+        let resultingTitle = await driver.findElement(By.xpath("/html/body/div/div/div[1]")).getText();
+        resultingTitle.should.equal(testingUpdate);
 
         await driver.quit();
     });
@@ -131,10 +124,8 @@ describe("Unit Tests", async function(){
         await driver.findElement(By.xpath("//*[@id=\"commentsubmit\"]")).click();
 
         //Test if comment has been added
-        // let commentContent = await driver.findElements(By.xpath("//*[contains(text(), \""+testComment.comment+"\")]"));
-        // let commentAuthor = await driver.findElements(By.xpath("//*[contains(text(), \""+testComment.author+"\")]"));
-        // commentContent.push().should.equal(testComment.comment);
-        // commentAuthor.push().should.equal(testComment.author);
+        let resultingComment = await driver.findElements(By.xpath("/html/body/div/div/div[5]"));
+        resultingComment.should.not.be.empty;
         
         await driver.quit();
     });
@@ -191,9 +182,30 @@ describe("Integration Testing", async function(){
         
         //Test if updates are reflected
         await driver.navigate().to("http://localhost:8008");
-        // let resultingTitle = await driver.findElement(By.xpath("/html/body/div/div/div[1]")).getText();
-        // resultingTitle.should.equal(testingUpdate);
+        let resultingTitle = await driver.findElement(By.xpath("/html/body/div/div/div[1]")).getText();
+        resultingTitle.should.equal(testingUpdate);
 
+        //Adds a comment
+        await driver.navigate().to("http://localhost:8008");
+        await driver.findElement(By.xpath("//*[contains(text(), \""+testingUpdate+"\")]")).click();
+
+        await driver.findElement(By.xpath("//*[@id=\"commentauthor\"]")).sendKeys(testComment.author);
+        await driver.findElement(By.xpath("//*[@id=\"commentarea\"]")).sendKeys(testComment.comment);
+        await driver.findElement(By.xpath("//*[@id=\"commentsubmit\"]")).click();
+
+        //Test if comment has been added
+        let resultingComment = await driver.findElements(By.xpath("/html/body/div/div/div[5]"));
+        resultingComment.should.not.be.empty;
+
+        //Delete the post test
+        await driver.findElement(By.xpath("//*[contains(text(), \""+testingUpdate+"\")]")).click();
+
+        await driver.findElement(By.xpath("//*[@id=\"deletepost\"]")).click();
+
+        //Test if post has been deleted in homepage
+        await driver.navigate().to("http://localhost:8008");
+        let resultingElement = await driver.findElements(By.xpath("//*[contains(text(), \""+testingUpdate+"\")]"));
+        resultingElement.should.be.empty;
 
         await driver.quit();
     })
